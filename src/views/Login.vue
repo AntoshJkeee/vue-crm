3c7ec9040ecbaf555100aa83d669bb6646d6e5cd
AntoshJkeee <toha_bart@mail.ru>
2023-01-31 19:26:24 +0300

    new content

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,12 @@
 <template>
-  <form class="card auth-card">
+  <form class="card auth-card" @submit.prevent="">
     <div class="card-content">
       <span class="card-title">Домашняя бухгалтерия</span>
       <div class="input-field">
         <input
             id="email"
             type="text"
+            class="validate"
         >
         <label for="email">Email</label>
         <small class="helper-text invalid">Email</small>
@@ -19,21 +20,6 @@
         <label for="password">Пароль</label>
         <small class="helper-text invalid">Password</small>
       </div>
-      <div class="input-field">
-        <input
-            id="name"
-            type="text"
-            class="validate"
-        >
-        <label for="name">Имя</label>
-        <small class="helper-text invalid">Name</small>
-      </div>
-      <p>
-        <label>
-          <input type="checkbox" />
-          <span>С правилами согласен</span>
-        </label>
-      </p>
     </div>
     <div class="card-action">
       <div>
@@ -41,14 +27,14 @@
             class="btn waves-effect waves-light auth-submit"
             type="submit"
         >
-          Зарегистрироваться
+          Войти
           <i class="material-icons right">send</i>
         </button>
       </div>
 
       <p class="center">
-        Уже есть аккаунт?
-        <a href="/">Войти!</a>
+        Нет аккаунта?
+        <router-link to="/register">Зарегистрироваться</router-link>
       </p>
     </div>
   </form>
